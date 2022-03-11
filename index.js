3d0e03024b5878d628fedba591f2f97a7500e3e4
ikbalrazu <iqbalraju123@gmail.com>
2022-03-11 20:59:33 +0600

    updated response

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ app.post("/twilioconfiq",(req,res)=>{
     console.log(twilioSID);
     console.log(twilioauthtoken);
     console.log(templatemessage);
+    let sendSMSinfo = [];
     for(let i=0;i<phonenumber.length;i++){
         const client = new twilio(twilioSID, twilioauthtoken);
         client.messages.create({
@@ -61,7 +62,9 @@ app.post("/twilioconfiq",(req,res)=>{
         .then(function(message){
             console.log(message.sid);
             var messagesid = message.sid;
-            res.json({status: "SMS Send Successfully", phone: phonenumber[i], twilionumber, twilioSID, twilioauthtoken, templatemessage, messagesid})
+            var SMSno = phonenumber[i];
+            //res.json({status: "SMS Send Successfully", phone: phonenumber[i], twilionumber, twilioSID, twilioauthtoken, templatemessage, messagesid})
+            sendSMSinfo.push({"msgSID":messagesid,"smsNumber":SMSno});
         });
     }
     // const client = new twilio(twilioSID, twilioauthtoken);
@@ -74,6 +77,7 @@ app.post("/twilioconfiq",(req,res)=>{
     //     .then((message) => console.log(message.sid));
     // console.log("hello world");
     //res.send({ status: "SMS Send Successfully", phonenumber, twilionumber, twilioSID, twilioauthtoken, templatemessage,messagesid});
+    res.send({status: "SMS Send Successfully",sendSMSinfo});
 
 })
 
